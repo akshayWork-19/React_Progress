f6677edf204a2fa33df539bd9d6f22fdfa7afc55
akshay kumar <akshay.1904kumar@gmail.com>
2025-10-02 13:10:01 +0530

    Successfully implemented to-do using context API

---
+++ todoContext/src/contexts/index.js
@@ -0,0 +1,3 @@
+export {
+  todoContext, TodoProvider, useTodo
+} from "./todoContext"
---
+++ todoContext/src/contexts/todoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const todoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo Message",
+      completed: false,
+    }
+  ],
+  addTodo: (todo) => { },
+  removeTodo: (id) => { },
+  updateTodo: (id, todo) => { },
+  toggleComplete: (id) => { }
+});
+
+
+export const useTodo = () => {
+  return useContext(todoContext);
+}
+
+export const TodoProvider = todoContext.Provider;
